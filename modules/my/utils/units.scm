4fb2884d33e68904dbec2dc319822bce708e8f95
arcensyl <dev@arcensyl.me>
2025-11-23 04:26:34 -0600

    Added more unit-related functions

--- modules/my/utils/units.scm
+++ modules/my/utils/units.scm
@@ -7,16 +7,16 @@
 
 (define-public (register-unit name applicator)
   (unless (or (symbol? name) (list-of-symbols? name))
-    (error "The name of a configuration unit must be a symbol or list of symbols"))
+    (error "A configuration unit's identifier must be a symbol or list of symbols"))
   
   (unless (procedure? applicator)
-    (error "The applicator of a configuration unit must be a procedure"))
+    (error "A configuration unit's applicator must be a procedure"))
 
   (hashq-set! registered-units name applicator))
 
 (define* (use-unit unit #:rest args)
   (unless (or (symbol? unit) (list-of-symbols? unit))
-    (error "Provide a symbol, or list of symbols, to specify the configuration unit to use"))
+    (error "A configuration unit's identifier must be a symbol or list of symbols"))
 
   (unless (hashq-ref registered-units unit)
     (error (format #f "Unit '~a' not found" unit)))
@@ -24,9 +24,27 @@
   (hashq-set! unit-queue unit args))
 (export use-unit)
 
+(define-public (ensure-unit unit)
+  (unless (or (symbol? unit) (list-of-symbols? unit))
+    (error "A configuration unit's identifier must be a symbol or list of symbols"))
+
+  (unless (hashq-ref registered-units unit)
+    (error (format #f "Configuration unit '~a' not found" unit)))
+
+  (let ((args (hashq-ref unit-queue unit)))
+    (if args
+        (hashq-set! unit-queue unit args)
+        (hashq-set! unit-queue unit '()))))
+
 (define-public (apply-all-units)
   (hash-for-each
    (lambda (unit args)
      (let ((applicator (hashq-ref registered-units unit)))
        (apply applicator args)))
    unit-queue))
+
+(define-public (using-unit? unit)
+  (unless (or (symbol? unit) (list-of-symbols? unit))
+    (error "A configuration unit's identifier must be a symbol or list of symbols"))
+
+  (if (hashq-get-handle registered-units unit) #t #f))
