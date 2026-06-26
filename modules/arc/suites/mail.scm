b31a09625b0f371ab139bed18ea40767292f7c69
arcensyl <dev@arcensyl.me>
2026-06-26 07:28:00 -0500

    Created basic mail suite

---
+++ modules/arc/suites/mail.scm
@@ -0,0 +1,34 @@
+;;; Copyright © 2025-2026 Arcensyl <dev@arcensyl.me>
+;;;
+;;; This file is NOT part of GNU Guix.
+;;;
+;;; This program is free software; you can redistribute it and/or modify it
+;;; under the terms of the GNU General Public License as published by the Free
+;;; Software Foundation; either version 3 of the License, or (at your option)
+;;; any later version.
+;;;
+;;; This program is distributed in the hope that it will be useful, but
+;;; WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+;;; or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+;;; for more details.
+;;;
+;;; You should have received a copy of the GNU General Public License along
+;;; with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+(define-module (arc suites mail)
+  #:use-module (arc core)
+  #:use-module (arc system nix)
+  #:use-module (arc util features))
+
+;; TODO: Write custom services so mail sync can be set up via Guix.
+;; TODO: Write a Guix package for the 'oama' tool.
+
+(define-feature mail
+  (feat-require 'nix)
+  
+  (use-home-packages
+   "isync"
+   "msmtp"
+   "mu")
+
+  (use-nix-packages "oama"))
