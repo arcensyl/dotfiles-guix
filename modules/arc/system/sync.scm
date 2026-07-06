00da8cb053442c641b5cb9efcd8d162c4af8141d
arcensyl <dev@arcensyl.me>
2026-07-06 17:32:35 -0500

    Fixed 'sync' module I implemented a hack to make Guix generate a config file using Syncthing's latest schema, as the default is currently pretty far out of date. Syncthing tries to automatically update its config file, but that causes conflicts with Guix Home's activation script.

--- modules/arc/system/sync.scm
+++ modules/arc/system/sync.scm
@@ -16,12 +16,38 @@
 ;;; with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 (define-module (arc system sync)
+  #:use-module (ice-9 regex)
   #:use-module (gnu home services)
   #:use-module (gnu home services syncthing)
   #:use-module (arc core)
   #:use-module (arc util features)
   #:use-module (arc util defer)
   #:use-module (arc util misc))
+
+;; HACK: The following code might be the most cursed thing I've written in Scheme.
+;; This fixes an issue where the Syncthing service's generated config uses an outdated schema.
+;; Syncthing will try and automatically update this config, causing conflicts with Guix.
+;; This hack avoids this by overloading the config generator so it does the migration itself.
+
+(let ((mod (resolve-module '(gnu services syncthing))))
+  (define serialize-syncthing-config-file/base
+    (module-ref mod 'serialize-syncthing-config-file))
+
+  (module-set! mod 'serialize-syncthing-config-file
+               (lambda (config)
+                 (let* ((r1 (make-regexp "<configuration version=\"37\">"))
+                        (r2 (make-regexp "</pullerPauseS>"))
+                        (r3 (make-regexp "</sendXattrs>"))
+
+                        (s1 "<configuration version=\"52\">")
+                        (s2 "</pullerPauseS><pullerDelayS>1</pullerDelayS>")
+                        (s3 "</sendXattrs><blockIndexing>true</blockIndexing>")
+                        
+                        (out (serialize-syncthing-config-file/base config))
+                        (out (regexp-substitute/global #f r1 out 'pre s1 'post))
+                        (out (regexp-substitute/global #f r2 out 'pre s2 'post))
+                        (out (regexp-substitute/global #f r3 out 'pre s3 'post)))
+                   out))))
 
 (define *sync-dir-queue* '())
 
@@ -43,4 +69,5 @@ DIR must be a <syncthing-folder> record."
                  (syncthing-config-file
                   (folders *sync-dir-queue*)
                   (usage-reporting-accepted -1)
+                  (gui-apikey "guix-syncthing-key")
                   (start-browser? #f)))))))))
